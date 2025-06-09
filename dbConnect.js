bc39a61af661a123f48adf4ae8457c6116d91c39
amreshkumar-web <121627475+amreshkumar-web@users.noreply.github.com>
2025-06-09 21:11:06 +0530

    db change commit

--- dbConnect.js
+++ dbConnect.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+
 const sequelize = new Sequelize(
   process.env.MYSQLDATABASE,
   process.env.MYSQLUSER,
   process.env.MYSQLPASSWORD,
   {
     host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+    port: process.env.MYSQLPORT || 3306,
     dialect: 'mysql'
   }
 );
-
 
 module.exports = sequelize;
